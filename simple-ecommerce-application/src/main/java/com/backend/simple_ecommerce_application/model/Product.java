24ed297c8853828437a0af2a639516547c03edbe
S.S.THORUNITHA <thorunitha.sathiyanarayanan@gmail.com>
2025-01-06 21:44:37 +0200

    Created Model Class

---
+++ simple-ecommerce-application/src/main/java/com/backend/simple_ecommerce_application/model/Product.java
@@ -0,0 +1,23 @@
+package com.backend.simple_ecommerce_application.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+
+    private Date releaseDate;
+    private boolean productAvailable;
+}
